47ac5d40c31ddd5f717c9008400d327f3eb7c71e
362515241010 <52314793+362515241010@users.noreply.github.com>
2019-07-05 14:13:47 +0700

    Add files via upload
    โปรแกรมแรก

---
+++ Starter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("  Tuey Shop VAT INVOICE ")
+print("---------------------------------")
+print("Water         x 1 : 20    THB")
+print("Ships          x 3 : 20    THB")
+print("Ice-cream   x 1 : 20    THB")
+print("Sock           x 1 : 20    THB")
+print("Sandwich    x 1 : 20    THB")
+print("---------------------------------")
+print("Total                  140  THB")
+int(input("   "))
+
+
+# In[13]:
+
+
+print(type(10))
+print(type("Hello"))
+print(type(10.3))
+print(type(2j))
+print(type(True))
+print(10+3.5)
+
+
+# In[ ]:
+
+
+
+
