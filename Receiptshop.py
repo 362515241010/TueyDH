e4d214ab164bf7a02d81ed7d7a4b138d7b253f12
362515241010 <52314793+362515241010@users.noreply.github.com>
2019-07-05 16:21:03 +0700

    My shop reciept
    ใบเสร็จเงิน

---
+++ Receiptshop.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[30]:
+
+
+print("     Welcome to Tuey shop")
+print("--------------------------------------")
+print("Bread        "           , 3 , 15 , "THB" , 3*15 , "THB")
+print("Chips         "           , 1 , 20 , "THB" , 1*20 , "THB")
+print("Milk           "           , 1 , 20 , "THB" , 1*20 , "THB")
+print("Sandwich   "           , 2 , 30 , "THB" , 2*30 , "THB")
+print("--------------------------------------")
+print("Total                      ",3*15+1*20+2*30, "THB" )
+print("--------------------------------------")
+print("             Thankyou")
+
+
+# In[ ]:
+
+
+
+
