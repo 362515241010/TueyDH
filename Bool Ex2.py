bcae465af6a5691ce47f68c376bf271b606da4b5
362515241010 <52314793+362515241010@users.noreply.github.com>
2019-07-05 16:17:29 +0700

    Boolean
    ตรรกะ

---
+++ Bool Ex2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+print(True)
+print(type(True))
+print(False)
+print(type(False))
+print(False and False)
+print(True or False)
+
+
+# In[ ]:
+
+
+
+
