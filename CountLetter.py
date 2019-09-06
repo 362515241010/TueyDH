b88dbce13f975aa0899ae8a10fa7bb1e3fd65adf
362515241010 <52314793+362515241010@users.noreply.github.com>
2019-09-06 17:27:09 +0700

    CL

---
+++ CountLetter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#นาย ปุณยวัจน์ เฟื่องฟุ้ง 362515241010 EE36241N
+n=0
+for w in input().strip() :
+    if w.isupper():
+        n+=1
+print(n)
+
+
+# In[ ]:
+
+
+
+
