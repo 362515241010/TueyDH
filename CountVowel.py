d9931e4a97bf224a2a628ad3bf0cc274dbce25e1
362515241010 <52314793+362515241010@users.noreply.github.com>
2019-09-06 17:26:55 +0700

    CV

---
+++ CountVowel.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#นาย ปุณยวัจน์ เฟื่องฟุ้ง 362515241010 EE36241N
+count=0
+for x in input():
+    if x=="a" or x=="e" or x=="i" or x=="o" or x=="u":
+        count+=1
+print(count)
+
+
+# In[ ]:
+
+
+
+
