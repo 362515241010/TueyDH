7de5979faae846ac1bf7161f029c147618258539
362515241010 <52314793+362515241010@users.noreply.github.com>
2019-09-22 08:52:53 +0700

    Add files via upload

---
+++ Home-Work-20-9-62_1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+#input จำนวนเงินเข้ามา
+money = float(input("Enter money"))
+#process เมื่อรับจำนวนเงินเข้ามาทางคีบอร์ดแล้วให้นำมาคิดภาษีโดยมีภาษีเท่ากับ 7 %
+#Define a variable กำหนดให้ตัวแปรทั้งหมดเป็นข้อมูลแบบ float
+tax = float(money*(7/100))
+#output เมื่อทำขั้นตอน process แล้วก็ทำการ output คำตอบออกมาด้วยคำสั่ง print
+print("tax =",tax)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
